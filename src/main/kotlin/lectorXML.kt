838afcca715654ad20e104afebe41d55148a56cc
agustrodmar <118189241+agustrodmar@users.noreply.github.com>
2023-10-09 19:38:29 +0200

    Update lectorXML.kt
    Modified several variables. Now code should be more readable.

--- src/main/kotlin/lectorXML.kt
+++ src/main/kotlin/lectorXML.kt
@@ -5,14 +5,14 @@ class lectorXML : DefaultHandler() {
     private var currentElement = ""
     private var content = StringBuilder() // Me permite concatenar cadenas de forma eficiente.
 
-    override fun startElement(uri: String, localName: String, qName: String, attributes: Attributes) {
-        currentElement = qName
+    override fun startElement(uri: String, localName: String, element: String, attributes: Attributes) {
+        currentElement = element
         // Añade una nueva linea por cada etiqueta que lee.
-        content.append("\n$qName: ")
+        content.append("\n$element: ")
     }
 
-    override fun endElement(uri: String, localName: String, qName: String) {
-        if (qName.equals("row", ignoreCase = true)) { // Le ordeno que ignore mayúsculas, necesario para este caso.
+    override fun endElement(uri: String, localName: String, element: String) {
+        if (element.equals("row", ignoreCase = true)) { // Le ordeno que ignore mayúsculas, necesario para este caso.
             // Imprime el contenido completo del registro cuando se encuentra una etiqueta de cierre "row"
             println("Contenido del registro:${content.toString().trim()}")
             println("============================")
